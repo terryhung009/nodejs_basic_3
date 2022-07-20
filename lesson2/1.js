0ee779aaed57dcbb3fc67763a956dd2a83274c83
denny <denny@curdr.tw>
2022-07-20 18:05:43 +0800

    update

---
+++ lesson2/1.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((request,response)=>{
+  response.end("Hello From NodeJS Server");
+
+});
+
+const port = 3000
+const ip = "127.0.0.1"
+
+server.listen(port, ip, ()=>{
+  console.log(`server is running at http://${ip}:${port}`);
+
+
+});
+
+
